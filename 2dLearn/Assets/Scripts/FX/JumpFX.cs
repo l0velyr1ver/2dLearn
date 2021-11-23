9d4f1fe9776e5578754d3a6d62d7a86bca84f888
l0velyr1ver <1138822516@qq.com>
2021-11-23 19:50:50 +0800

    修改同上

---
+++ 2dLearn/Assets/Scripts/FX/JumpFX.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpFX : MonoBehaviour
+{
+    public void Finish()
+    {
+        gameObject.SetActive(false);
+    }
+}
